463586cb7c325a9a7939b220181c00455e6ec913
Lucas Muniz <lucasmuniz.pro@hotmail.com>
2025-03-07 09:03:22 -0300

    Update code_project_02.R

--- code_project_02/code_project_02.R
+++ code_project_02/code_project_02.R
@@ -6,23 +6,35 @@ library('nycflights13')
 # Based on the Flights library, let's create a new dataset called pop_data,
 # referring to the flights of the airlines UA (United Airlines) and DL (Delta Airlines).
 # The dataset will have only two columns (company name and arrival flight delay).
-pop_data = na.omit(flights) %>% filter(carrier == 'UA' | carrier == 'DL', arr_delay >= 0) 
-%>% select(carrier, arr_delay) %>% group_by(carrier) %>% sample_n(17000) %>% ungroup()
+pop_data <- na.omit(flights) %>% 
+  filter(carrier %in% c('UA', 'DL'), arr_delay >= 0) %>% 
+  select(carrier, arr_delay) %>% 
+  group_by(carrier) %>% 
+  sample_n(17000) %>% 
+  ungroup()
 
 # Creation of two samples with 1000 observations each from the pop_data dataset.
 # Let's establish sample 1 with data from the company Delta Airlines (DL)
 # Let's establish sample 2 with data from the company United Airlines (UA)
 # And at the end join both samples in a single dataset
-amostra1 = na.omit(pop_data) %>% select(carrier, arr_delay) %>% filter(carrier == 'DL') 
-%>% mutate(sample_id = '1') %>% sample_n(1000)
-amostra2 = na.omit(pop_data) %>% select(carrier, arr_delay) %>% filter(carrier == 'UA') 
-%>% mutate(sample_id = '2') %>% sample_n(1000)
-samples = rbind(amostra1,amostra2)
+amostra1 <- na.omit(pop_data) %>% 
+  select(carrier, arr_delay) %>% 
+  filter(carrier == 'DL') %>% 
+  mutate(sample_id = '1') %>% 
+  sample_n(1000)
+
+amostra2 <- na.omit(pop_data) %>% 
+  select(carrier, arr_delay) %>% 
+  filter(carrier == 'UA') %>% 
+  mutate(sample_id = '2') %>% 
+  sample_n(1000)
+
+samples <- rbind(amostra1, amostra2)
 
 # In this step, we will calculate the confidence interval (95%) for samples 1 and 2.
 # Standard error
-erro_padrao_amostra1 = sd(amostra1$arr_delay) / sqrt(nrow(amostra1))
-erro_padrao_amostra2 = sd(amostra2$arr_delay) / sqrt(nrow(amostra2))
+erro_padrao_amostra1 <- sd(amostra1$arr_delay) / sqrt(nrow(amostra1))
+erro_padrao_amostra2 <- sd(amostra2$arr_delay) / sqrt(nrow(amostra2))
 
 # This formula is used to calculate the standard deviation of a sample mean distribution
 # (of a large number of samples from a population). In short, it is applicable when
@@ -31,15 +43,15 @@ erro_padrao_amostra2 = sd(amostra2$arr_delay) / sqrt(nrow(amostra2))
 
 # Lower and upper limits
 # 1.96 is the z score value for 95% confidence
-lower1 = mean(amostra1$arr_delay) - 1.96 * erro_padrao_amostra1  
-upper1 = mean(amostra1$arr_delay) + 1.96 * erro_padrao_amostra1
-lower2 = mean(amostra2$arr_delay) - 1.96 * erro_padrao_amostra2
-upper2 = mean(amostra2$arr_delay) + 1.96 * erro_padrao_amostra2
+lower1 <- mean(amostra1$arr_delay) - 1.96 * erro_padrao_amostra1  
+upper1 <- mean(amostra1$arr_delay) + 1.96 * erro_padrao_amostra1
+lower2 <- mean(amostra2$arr_delay) - 1.96 * erro_padrao_amostra2
+upper2 <- mean(amostra2$arr_delay) + 1.96 * erro_padrao_amostra2
 
 # confidence interval
-ic_1 = c(lower1,upper1)
+ic_1 <- c(lower1, upper1)
 mean(amostra1$arr_delay)
-ic_2 = c(lower2,upper2)
+ic_2 <- c(lower2, upper2)
 mean(amostra2$arr_delay)
 
 # Sample 1 has an average flight delay of DE 39,299 and a confidence interval
@@ -49,11 +61,13 @@ mean(amostra2$arr_delay)
 # from about (31.34 to 37.33), indicating that the mean is fully in.
 
 # Creating a graph to visualize the previously created confidence intervals
-toPlot = summarise(group_by(samples, sample_id), mean = mean(arr_delay))
-toPlot = mutate(toPlot, lower = ifelse(toPlot$sample_id == 1,ic_1[1],ic_2[1]))
-toPlot = mutate(toPlot, upper = ifelse(toPlot$sample_id == 1,ic_1[2],ic_2[2]))
-ggplot(toPlot, aes(x = sample_id, y=mean, colour = sample_id )) + 
-geom_point() + geom_errorbar(aes(ymin=lower, ymax=upper), width=.1)
+toPlot <- summarise(group_by(samples, sample_id), mean = mean(arr_delay))
+toPlot <- mutate(toPlot, lower = ifelse(toPlot$sample_id == 1, ic_1[1], ic_2[1]))
+toPlot <- mutate(toPlot, upper = ifelse(toPlot$sample_id == 1, ic_1[2], ic_2[2]))
+
+ggplot(toPlot, aes(x = sample_id, y = mean, colour = sample_id)) + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = lower, ymax = upper), width = .1)
 
 # Based on the data developed so far, it is possible to say that most likely,
 # the samples came from the same population, due to the fact that most of the data resides
@@ -73,26 +87,26 @@ dl <- sample_n(filter(pop_data, carrier == "DL", arr_delay > 0), 1000)
 ua <- sample_n(filter(pop_data, carrier == "UA", arr_delay > 0), 1000)
 
 # Calculate standard error and mean
-se = sd(dl$arr_delay) / sqrt(nrow(dl))
+se <- sd(dl$arr_delay) / sqrt(nrow(dl))
 mean(dl$arr_delay)
 
 # Lower and upper limits
-lower = mean(dl$arr_delay) - 1.96 * se
-upper = mean(dl$arr_delay) + 1.96 * se
-ic_dl = c(lower,upper)
+lower <- mean(dl$arr_delay) - 1.96 * se
+upper <- mean(dl$arr_delay) + 1.96 * se
+ic_dl <- c(lower, upper)
 ic_dl
 
 # Repeat the process for the other company
-se = sd(ua$arr_delay) / sqrt(nrow(ua))
+se <- sd(ua$arr_delay) / sqrt(nrow(ua))
 mean(ua$arr_delay)
 
-lower = mean(ua$arr_delay) - 1.96 * se
-upper = mean(ua$arr_delay) + 1.96 * se
-ic_ua = c(lower,upper)
+lower <- mean(ua$arr_delay) - 1.96 * se
+upper <- mean(ua$arr_delay) + 1.96 * se
+ic_ua <- c(lower, upper)
 ic_ua
 
 # Students t-test
-t.test(dl$arr_delay, ua$arr_delay, alternative="greater")
+t.test(dl$arr_delay, ua$arr_delay, alternative = "greater")
 
 # The p-value is a quantification of the probability of making a mistake in rejecting H0,
 # resulting from the adopted statistical distribution. If the p-value is less than the
